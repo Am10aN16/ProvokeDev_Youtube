a4fee2467e5e385b88f5f70f2dde2344bf572faf
Aman Kr. Poddar <76661001+Am10aN16@users.noreply.github.com>
2023-12-16 10:03:15 +0530

    Add files via upload
    Deployed

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,13 @@
+import React ,{useState , useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import{Box} from '@mui/material'
+
+import {Videos , channelCard} from './';
+
+const ChannelDetail = () => {
+  return (
+    <div>ChannelDetail</div>
+  )
+}
+
+export default ChannelDetail
